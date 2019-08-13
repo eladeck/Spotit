65e6bd030d2a910556a11fd0edb49ea249438de1
Elad Eckstein <eladeck@gmail.com>
2019-08-13 21:56:33 +0300

    shuffle the image on the feed!

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -52,9 +52,20 @@ class Feed extends Component {
                 )
             });
             
-            return imageWrappers;
+            return this.shuffleArray(imageWrappers);
           } // else
     } // importImages
+
+    shuffleArray(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
 
     render() {
         const images = this.importImages();
