2e74d882da9d8df3afe1c7aa6dadb742477e42b8
Elad Eckstein <eleckste@microsoft.com>
2019-09-15 09:06:41 +0300

    removedd polyfill from webpack.config.js entry propery

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 
 module.exports = {
-  entry: ["@babel/polyfill","./src/index.js"],
+  entry: ["./src/index.js"],
   output: {
     filename: "bundle.js",
     path: resolve(__dirname, "public")
